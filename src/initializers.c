f346d6e9884fa7419b437cacf71c56243310f85e
Alexandra Vachon <alvachon@student.42quebec.com>
2023-06-05 11:48:52 -0400

    init windows + pull update

--- src/initializers.c
+++ src/initializers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   initializers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
+/*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 12:50:45 by llord             #+#    #+#             */
-/*   Updated: 2023/05/22 14:10:30 by llord            ###   ########.fr       */
+/*   Updated: 2023/06/05 11:47:24 by alvachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,12 @@ void	init_map(void)
 	connect_map(); //			connects all the map's tiles into a tile grid
 	flood_check_map(); //		verifies that the map is closed around the player
 //	init_player() //			creates the player's entity
+}
+
+void	init_window(void)
+{
+	t_master	*data;
+
+	data = get_master();
+	data->window = mlx_init(2048, 1024, "CUBE3D", true);
 }
