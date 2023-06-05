2fa2e879a4b6ff68bc0c6101add064a8d043041a
Alexandra Vachon <alvachon@student.42quebec.com>
2023-06-05 14:14:11 -0400

    ecran imprime

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: alvachon <alvachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 08:55:54 by llord             #+#    #+#             */
-/*   Updated: 2023/06/05 13:48:56 by alvachon         ###   ########.fr       */
+/*   Updated: 2023/06/05 14:12:19 by alvachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,7 @@ void	init_game(int ac, char	**av)
 	init_map(); //					creates the map grid from the map-info contained in d.level
 
 //	print_tiles(); //								0============ DEBUG ============0
-
-//	init_window();
-
+	init_window();
 	print_paths(); //								0============ DEBUG ============0
 	print_colours(); //								0============ DEBUG ============0
 
